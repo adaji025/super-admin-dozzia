312d2dba4b2e5b699d8bdead5c6d65bf2ad56744
Opeyemi Oyeleke <topfemo@gmail.com>
2022-04-03 07:27:29 +0100

    minor ui changes

--- src/components/AuthHeader/AuthHeader.tsx
+++ src/components/AuthHeader/AuthHeader.tsx
@@ -9,7 +9,10 @@ const AuthHeader = () => {
   const dark = colorScheme === "dark";
 
   return (
-    <div className="auth-header">
+    <div
+      className="auth-header"
+      style={{ borderBottom: `1px solid ${dark ? "#2b2f32" : "#e9ecef"}` }}
+    >
       <div className="app-logo">
         <Link to="/">
           <Text style={{ fontSize: "30px" }}>Dozzia</Text>
