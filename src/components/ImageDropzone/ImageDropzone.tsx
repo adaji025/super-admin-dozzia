2e61975628047bbf786dffe220abc73cf85aa3b5
Opeyemi Oyeleke <topfemo@gmail.com>
2022-04-07 08:08:52 +0100

    add image dropzone component

---
+++ src/components/ImageDropzone/ImageDropzone.tsx
@@ -0,0 +1,79 @@
+import { Group, Text, useMantineTheme, MantineTheme } from "@mantine/core";
+import { Upload, Photo, X, Icon as TablerIcon } from "tabler-icons-react";
+import { Dropzone, DropzoneStatus, IMAGE_MIME_TYPE } from "@mantine/dropzone";
+
+function getIconColor(status: DropzoneStatus, theme: MantineTheme) {
+  return status.accepted
+    ? theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 4 : 6]
+    : status.rejected
+    ? theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]
+    : theme.colorScheme === "dark"
+    ? theme.colors.dark[0]
+    : theme.colors.gray[7];
+}
+
+function ImageUploadIcon({
+  status,
+  ...props
+}: React.ComponentProps<TablerIcon> & { status: DropzoneStatus }) {
+  if (status.accepted) {
+    return <Upload {...props} />;
+  }
+
+  if (status.rejected) {
+    return <X {...props} />;
+  }
+
+  return <Photo {...props} />;
+}
+
+export const dropzoneChildren = (
+  status: DropzoneStatus,
+  theme: MantineTheme,
+  filetype: string,
+  fileLimit: string
+) => (
+  <Group
+    position="center"
+    spacing="sm"
+    style={{ minHeight: 70, pointerEvents: "none" }}
+  >
+    <ImageUploadIcon
+      status={status}
+      style={{ color: getIconColor(status, theme) }}
+      size={40}
+    />
+
+    <div>
+      <Text size="lg" align="center">
+        Drag {filetype} here or click to select
+      </Text>
+      <Text align="center" size="sm" color="dimmed" inline mt={7}>
+        Each file should not exceed {fileLimit}
+      </Text>
+    </div>
+  </Group>
+);
+
+const ImageDropzone = ({
+  filetype,
+  fileLimit,
+}: {
+  filetype: string;
+  fileLimit: string;
+}) => {
+  const theme = useMantineTheme();
+  return (
+    <Dropzone
+      onDrop={(files) => console.log("accepted files", files)}
+      onReject={(files) => console.log("rejected files", files)}
+      maxSize={3 * 1024 ** 2}
+      accept={IMAGE_MIME_TYPE}
+      disabled
+    >
+      {(status) => dropzoneChildren(status, theme, filetype, fileLimit)}
+    </Dropzone>
+  );
+};
+
+export default ImageDropzone;
