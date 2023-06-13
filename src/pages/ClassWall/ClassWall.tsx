253fe9cef0af21aa4e1e6077d6032b2c0c3f3a79
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2023-06-13 14:53:58 +0100

    Class wall refactor 2 (#31)
    * pulled from main
    
    * refactor class-wall ui
    
    * made corrections to class wall refactor
    
    * made adjustmentt the classwall page
    
    * cleanup
    
    * fix page tittle font weight and size
    
    ---------
    
    Co-authored-by: Opeyemi Oyeleke <topfemo@gmail.com>

--- src/pages/ClassWall/ClassWall.tsx
+++ src/pages/ClassWall/ClassWall.tsx
@@ -8,6 +8,22 @@ import useClass from "../../hooks/useClass";
 import { Button, Menu } from "@mantine/core";
 import { ChevronDown, ArrowLeft } from "tabler-icons-react";
 import { showNotification } from "@mantine/notifications";
+import RelaxedMascot from "../../assets/svg/relaxed-mascot.svg";
+
+import AcademicLog from "../../assets/svg/academic-log.svg";
+import BehavioralLog from "../../assets/svg/behavioral-log.svg";
+import TestsExams from "../../assets/svg/tests-exams.svg";
+import StudyResources from "../../assets/svg/study-resources.svg";
+import ClassEvent from "../../assets/svg/class-event.svg";
+import Curriculum from "../../assets/svg/curriculum.svg";
+
+import AcademicLogIcon from "../../assets/svg/academic-log-icon.svg";
+import BehavioralLogIcon from "../../assets/svg/behavioral-log-icon.svg";
+import TestsExamsIcon from "../../assets/svg/test-exams-icon.svg";
+import StudyResourcesIcon from "../../assets/svg/study-resources-icon.svg";
+import ClassEventIcon from "../../assets/svg/class-event-icon.svg";
+import CurriculumIcon from "../../assets/svg/curriculum-icon.svg";
+
 import "./class-wall.scss";
 import "../Classes/classes.scss";
 
@@ -58,12 +74,42 @@ const ClassWall = () => {
   };
 
   const classWallItems = [
-    { name: "Academic Log", route: "/class-wall/academic-log" },
-    { name: "Behavioral Log", route: "/class-wall/behavioral-log" },
-    { name: "Test/Exam", route: "/class-wall/test-exam" },
-    { name: "Study Resources", route: "/class-wall/study-resources" },
-    { name: "Class Events", route: "/class-wall/class-events" },
-    { name: "Curriculum", route: "/class-wall/curriculum" },
+    {
+      name: "Academic Log",
+      route: "/class-wall/academic-log",
+      ellipse: AcademicLog,
+      icon: AcademicLogIcon,
+    },
+    {
+      name: "Behavioral Log",
+      route: "/class-wall/behavioral-log",
+      ellipse: BehavioralLog,
+      icon: BehavioralLogIcon,
+    },
+    {
+      name: "Tests and Exams",
+      route: "/class-wall/test-exam",
+      ellipse: TestsExams,
+      icon: TestsExamsIcon,
+    },
+    {
+      name: "Study Resources",
+      route: "/class-wall/study-resources",
+      ellipse: StudyResources,
+      icon: StudyResourcesIcon,
+    },
+    {
+      name: "Class Events",
+      route: "/class-wall/class-events",
+      ellipse: ClassEvent,
+      icon: ClassEventIcon,
+    },
+    {
+      name: "Curriculum",
+      route: "/class-wall/curriculum",
+      ellipse: Curriculum,
+      icon: CurriculumIcon,
+    },
   ];
 
   return (
@@ -141,23 +187,34 @@ const ClassWall = () => {
 
         <div
           className="class-wall-main"
-          style={{ background: dark ? "#121212" : "#f6f6f6fc" }}
+          style={{ background: dark ? "#121212" : "" }}
         >
           {classWallItems.map((item, index) => (
-            <div className="c-card" key={index}>
-              <div
-                className="c-c-inner click no-select"
-                style={{
-                  background: dark ? "#1a1b1e" : "#ffffff",
-                }}
-                onClick={() => {
-                  handleWallItemSelect(item.route);
-                }}
-              >
-                {item.name}
+            <div
+              className={`c-card card-${index + 1} click`}
+              key={index}
+              onClick={() => {
+                handleWallItemSelect(item.route);
+              }}
+            >
+              <div className="inner-c-card">
+                <div className="inner-c-card-icon">
+                  <img src={item.icon} alt="" />
+                </div>
+                <div className="inner-c-card-text">{item.name}</div>
+                <div className="ellipse">
+                  <img src={item.ellipse} alt="" />
+                </div>
               </div>
             </div>
           ))}
+        </div>
+
+        <div className="mascot-container relative">
+          <img src={RelaxedMascot} alt="" />
+          <div className="tool-tip">
+            Relax and let Dozzia manage your school’s reports and activities
+          </div>
         </div>
       </div>
     </Fragment>
