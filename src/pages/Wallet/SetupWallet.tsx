4640cd2dc3eef262b9009cd72e05327c76156f9e
Opeyemi Oyeleke <topfemo@gmail.com>
2023-06-26 08:59:32 +0100

    cleanup

--- src/pages/Wallet/SetupWallet.tsx
+++ src/pages/Wallet/SetupWallet.tsx
@@ -1,100 +1,121 @@
-import { Button, Divider, Drawer, NumberInput, ScrollArea, Select, Text, TextInput } from '@mantine/core'
+import {
+  Button,
+  Divider,
+  Drawer,
+  NumberInput,
+  ScrollArea,
+  Select,
+  Text,
+  TextInput,
+} from "@mantine/core";
 
 interface SetupWalletProps {
-    drawerOpen: boolean
-    close: () => void
-    openSuccessModal: React.Dispatch<React.SetStateAction<boolean>>
-    callback: () => void
+  drawerOpen: boolean;
+  close: () => void;
+  openSuccessModal: React.Dispatch<React.SetStateAction<boolean>>;
+  callback: () => void;
 }
 
-const SetupWallet = ({ drawerOpen, callback, close, openSuccessModal }: SetupWalletProps) => {
-    return (
-        <Drawer
-            opened={drawerOpen}
-            withinPortal={false}
-            onClose={close}
-            padding="sm"
-            position="right"
-            size={400}
-            className='wallet-drawer'>
-            {drawerOpen &&
-                <ScrollArea>
-                    <span className="title">Set up your Dozzia wallet</span>
-                    <Divider my={20} />
-                    
-                    <span className="title">Primary bank account</span>
-                    <span className="acc-title">
-                        Add your school’s primary bank account details
-                    </span>
+const SetupWallet = ({
+  drawerOpen,
+  callback,
+  close,
+  openSuccessModal,
+}: SetupWalletProps) => {
+  return (
+    <Drawer
+      opened={drawerOpen}
+      onClose={close}
+      padding="lg"
+      position="right"
+      size={400}
+      className="wallet-drawer"
+      title={<Text className="title">Set up your Dozzia wallet</Text>}
+    >
+      {drawerOpen && (
+        <ScrollArea className="drawer-scroll-container" type="auto">
+          <Divider mb={20} />
 
-                    <form className="wallet-form">
-                        <Select
-                            size="sm"
-                            label="Select bank"
-                            placeholder="Select receiver’s bank"
-                            data={[
-                                { value: "gtb", label: "GTB" },
-                                { value: "keystone", label: "Keytone" },
-                                { value: "first bank", label: "First Bank" },
-                                { value: "wema", label: "Wema" },
-                            ]}
-                        />
+          <div className="title">Primary bank account</div>
+          <div className="acc-title">
+            Add your school’s primary bank account details
+          </div>
 
-                        <NumberInput
-                            size="sm"
-                            mt="sm"
-                            hideControls
-                            label="Account number"
-                        />
+          <form className="wallet-form">
+            <Select
+              size="sm"
+              label="Select bank"
+              placeholder="Select receiver’s bank"
+              data={[
+                { value: "gtb", label: "GTB" },
+                { value: "keystone", label: "Keytone" },
+                { value: "first bank", label: "First Bank" },
+                { value: "wema", label: "Wema" },
+              ]}
+            />
 
-                        <TextInput
-                            size="md"
-                            mt="sm"
-                            label="Account name"
-                            placeholder="Enter BVN"
-                        />
+            <NumberInput
+              size="sm"
+              mt="sm"
+              hideControls
+              label="Account number"
+            />
 
-                        <span className="title">Card details</span>
-                        <span className="card-details-desc">
-                            Add your school’s primary bank account details
-                        </span>
+            <TextInput
+              size="md"
+              mt="sm"
+              label="Account name"
+              placeholder="Enter BVN"
+            />
 
-                        <NumberInput
-                            size="sm"
-                            hideControls
-                            label="Card number"
-                            placeholder="0000 0000 0000 0000"
-                        />
+            <div className="title with-mt">Card details</div>
+            <div className="acc-title">
+              Add your school’s primary bank account details
+            </div>
 
-                        <div className="card-details-inputs">
-                            <NumberInput
-                                size="sm"
-                                mt="sm"
-                                hideControls
-                                label="Exp date name"
-                                placeholder="MM /  YY"
-                            />
+            <NumberInput
+              size="sm"
+              mt={24}
+              hideControls
+              label="Card number"
+              placeholder="0000 0000 0000 0000"
+            />
 
-                            <NumberInput
-                                size="sm"
-                                mt="sm"
-                                hideControls
-                                label="Account name"
-                                placeholder="Security code"
-                            />
-                        </div>
+            <div className="card-details-inputs">
+              <NumberInput
+                size="sm"
+                mt="sm"
+                hideControls
+                label="Exp date name"
+                placeholder="MM /  YY"
+              />
 
-                        <div className="btn">
-                            <Button size="md" mt="lg"
-                            onClick={() => openSuccessModal(true)}>
-                                Save
-                            </Button>
-                        </div>
-                    </form>
+              <NumberInput
+                size="sm"
+                mt="sm"
+                hideControls
+                label="Account name"
+                placeholder="Security code"
+              />
+            </div>
 
-                </ScrollArea>}
-        </Drawer>
-    )
-}
+            <div className="btn">
+              <Button
+                size="md"
+                mt="lg"
+                onClick={() => {
+                  close();
+                  openSuccessModal(true);
+                }}
+              >
+                Save
+              </Button>
+            </div>
+          </form>
+        </ScrollArea>
+      )}
+    </Drawer>
+  );
+};
 
-export default SetupWallet
+export default SetupWallet;
