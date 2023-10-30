65a5770a8b0f7c26766367a9a0c9fc4449bae38b
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2023-10-30 18:41:55 +0100

    added the new onboarding screen

--- src/components/Loggedin/Sidebar.tsx
+++ src/components/Loggedin/Sidebar.tsx
@@ -46,8 +46,8 @@ const Sidebar = ({ toggleSidebar, showSidebar }: SidebarProps) => {
 
   useEffect(() => {
     if (
-      userdata.role.name === Roles.Principal ||
-      userdata.role.name === Roles.SchoolAdmin
+      userdata?.role?.name === Roles.Principal ||
+      userdata?.role?.name === Roles.SchoolAdmin
     ) {
       setRoutes(adminRoutes);
     } else if (userdata?.role?.name === Roles.Teacher) {
@@ -56,6 +56,8 @@ const Sidebar = ({ toggleSidebar, showSidebar }: SidebarProps) => {
 
     //eslint-disable-next-line
   }, []);
+
+  console.log(userdata.role?.name)
 
   const adminRoutes = [
     {
